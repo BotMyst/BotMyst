192ce970b4ee197b6b66284423e1b51094f003e6
Leon Laci <leonlaci@outlook.com>
2017-10-05 13:18:28 +0200

    BotMyst will now connect to a server and appear online.

--- Program.cs
+++ Program.cs
@@ -4,9 +4,10 @@ namespace BotMyst
 {
     public class Program
     {
-        private static void Main (string [] args)
+        private static void Main ()
         {
-            
+            BotMyst bot = new BotMyst ();
+            bot.Start ().GetAwaiter ().GetResult ();
         }
     }
 }
