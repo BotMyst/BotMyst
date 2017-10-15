4e71223554132d38d5c9a4295d3aa04e7af595a3
Leon Laci <leonlaci@outlook.com>
2017-10-15 13:10:40 +0200

    You can now add commands

--- BotMyst.cs
+++ BotMyst.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Reflection;
 using System.Threading.Tasks;
 
 using Discord;
@@ -8,11 +9,17 @@ using Discord.WebSocket;
 
 using Newtonsoft.Json;
 
+using Microsoft.Extensions.DependencyInjection;
+
 namespace BotMyst
 {
     public class BotMyst
     {
         private DiscordSocketClient client;
+        private CommandService commands;
+        private IServiceProvider services;
+
+        private string commandPrefix = ">";
 
         private BotMystConfiguration config;
         private string token;
@@ -20,6 +27,7 @@ namespace BotMyst
         public async Task Start ()
         {
             client = new DiscordSocketClient ();
+            commands = new CommandService ();
 
             // Deserialize the config file and get the token (don't wanna hard code it for obvious reasons)
             using (StreamReader reader = new StreamReader ("BotMystConfig.json"))
@@ -29,6 +37,10 @@ namespace BotMyst
                 token = config.Token;
             }
 
+            services = new ServiceCollection ().BuildServiceProvider ();
+
+            await InstallCommands ();
+
             await client.LoginAsync (TokenType.Bot, config.Token);
             await client.SetGameAsync ("");
             await client.StartAsync ();
@@ -36,6 +48,26 @@ namespace BotMyst
             Console.WriteLine ("BotMyst is ready.");
 
             await Task.Delay (-1);
+        }
+
+        private async Task InstallCommands ()
+        {
+            client.MessageReceived += HandleCommand;
+            await commands.AddModulesAsync (Assembly.GetEntryAssembly ());
+        }
+
+        private async Task HandleCommand (SocketMessage msg)
+        {
+            SocketUserMessage message = msg as SocketUserMessage;
+            if (message == null) return;
+            int argPos = 0;
+
+            if ((message.HasStringPrefix (commandPrefix, ref argPos) || message.HasMentionPrefix (client.CurrentUser, ref argPos)) == false) return;
+
+            CommandContext context = new CommandContext (client, message);
+            IResult result = await commands.ExecuteAsync (context, argPos, services);
+            if (result.IsSuccess == false)
+                await context.Channel.SendMessageAsync (result.ErrorReason);
         }
     }
 }
