19e4f33dd2302ad0af2347c3727a8d9368ad8aa4
CodeMyst <codemyst@outlook.com>
2018-04-05 19:25:50 +0200

    Adding and removing of roles

---
+++ Commands/GiveRoleCommand.cs
@@ -0,0 +1,97 @@
+using System;
+using System.Net;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Discord;
+using Discord.Net;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace BotMyst.Commands
+{
+    public class GiveRoleCommand : ModuleBase
+    {
+        [Command ("giverole"), Summary ("Gives you the specified role (if exists)."), Alias ("gimme")]
+        [RequireBotPermission (GuildPermission.ManageRoles)]
+        [RequireContext (ContextType.Guild)]
+        public async Task GiveRole ([Remainder] IRole role)
+        {
+            IUser user = Context.User;
+            SocketGuildUser guildUser = user as SocketGuildUser;
+
+            if (guildUser.Roles.Contains (role))
+            {
+                EmbedBuilder goldB = new EmbedBuilder ();
+                goldB.WithColor (Color.Gold);
+                goldB.WithDescription ($"{user.Mention}, you already have the **{role.Name}** role.");
+                await ReplyAsync (string.Empty, false, goldB);
+                return;
+            }
+
+            try
+            {
+                await guildUser.AddRoleAsync (role);
+            }
+            catch (HttpException e)
+            {
+                if (e.HttpCode == HttpStatusCode.Forbidden)
+                {
+                    EmbedBuilder errorB = new EmbedBuilder ();
+                    errorB.WithColor (Color.Red);
+                    errorB.WithTitle ("Error");
+                    errorB.WithDescription ("I cannot do that.");
+                    await ReplyAsync (string.Empty, false, errorB);
+                }
+                return;
+            }
+
+            EmbedBuilder eb = new EmbedBuilder ();
+            eb.WithColor (Color.Green);
+            eb.WithTitle ("Success");
+            eb.WithDescription ($"{user.Mention}, you have been given the **{role.Name}** role.");
+            await ReplyAsync (string.Empty, false, eb);
+        }
+
+        [Command ("removerole"), Summary ("Removes the specified role (if you have it).")]
+        [RequireBotPermission (GuildPermission.ManageRoles)]
+        [RequireContext (ContextType.Guild)]
+        public async Task RemoveRole ([Remainder] IRole role)
+        {
+            IUser user = Context.User;
+            SocketGuildUser guildUser = user as SocketGuildUser;
+
+            if (guildUser.Roles.Contains (role) == false)
+            {
+                EmbedBuilder goldB = new EmbedBuilder ();
+                goldB.WithColor (Color.Gold);
+                goldB.WithDescription ($"{user.Mention}, you don't have the **{role.Name}** role.");
+                await ReplyAsync (string.Empty, false, goldB);
+                return;
+            }
+
+            try
+            {
+                await guildUser.RemoveRoleAsync (role);
+            }
+            catch (HttpException e)
+            {
+                if (e.HttpCode == HttpStatusCode.Forbidden)
+                {
+                    EmbedBuilder errorB = new EmbedBuilder ();
+                    errorB.WithColor (Color.Red);
+                    errorB.WithTitle ("Error");
+                    errorB.WithDescription ("I cannot do that.");
+                    await ReplyAsync (string.Empty, false, errorB);
+                }
+                return;
+            }
+
+            EmbedBuilder eb = new EmbedBuilder ();
+            eb.WithColor (Color.Green);
+            eb.WithTitle ("Success");
+            eb.WithDescription ($"{user.Mention}, **{role.Name}** role has been removed from you.");
+            await ReplyAsync (string.Empty, false, eb);
+        }
+    }
+}
