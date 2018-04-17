a3a3410a682d4cfd678b597d0e81a5ac9a30fda6
CodeMyst <codemyst@outlook.com>
2018-04-17 19:46:58 +0200

    Register all available commands to the server

---
+++ Data/Module.cs
@@ -0,0 +1,11 @@
+namespace BotMyst.Data
+{
+    public class Module
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool Enabled { get; set; }
+        public string Options { get; set; }
+    }
+}
