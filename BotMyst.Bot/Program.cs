ad056d6ad095b6f5568040995e636463a024b991
CodeMyst <codemyst@outlook.com>
2018-04-28 12:55:11 +0200

    v2 init

---
+++ BotMyst.Bot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BotMyst.Bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
