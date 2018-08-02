530d3671e72d793999936af9c4725d50376b1f75
CodeMyst <codemyst@outlook.com>
2018-08-02 19:03:51 +0200

    Closing of the role picker

--- BotMyst.Web/wwwroot/scripts/main.js
+++ BotMyst.Web/wwwroot/scripts/main.js
@@ -2,3 +2,8 @@ function doSomething (guildId, commandId, optionName)
 {
     $('#rolePicker').load ('CommandSettings/DisplayRolePicker', { guildId: guildId, commandId: commandId, optionName: optionName });
 }
+
+function closeRolePicker ()
+{
+    $('#rolePicker').empty ();
+}
