5a314faffcc872e7c27058520910597e0297c33b
CodeMyst <codemyst@outlook.com>
2018-08-02 19:41:50 +0200

    Added a transition to the role picker

--- BotMyst.Web/wwwroot/scripts/main.js
+++ BotMyst.Web/wwwroot/scripts/main.js
@@ -1,9 +1,17 @@
-function doSomething (guildId, commandId, optionName)
+function displayRolePicker (guildId, commandId, optionName)
 {
     $('#rolePicker').load ('CommandSettings/DisplayRolePicker', { guildId: guildId, commandId: commandId, optionName: optionName });
+    setTimeout (function ()
+    {
+        $('#rolePicker').addClass ('visible');
+    }, 200);
 }
 
 function closeRolePicker ()
 {
-    $('#rolePicker').empty ();
+    $('#rolePicker').removeClass ('visible');
+    setTimeout (function ()
+    {
+        $('#rolePicker').empty ();
+    }, 200);
 }
