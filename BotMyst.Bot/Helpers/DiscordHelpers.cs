aa4ad4918a80bd4fa932dc65ce176fd492670fef
CodeMyst <codemyst@outlook.com>
2018-08-18 20:42:43 +0200

    Moved CheckWhitelistBlacklistRoles into BotMystHelpers class

--- BotMyst.Bot/Helpers/DiscordHelpers.cs
+++ BotMyst.Bot/Helpers/DiscordHelpers.cs
@@ -47,44 +47,5 @@ namespace BotMyst.Bot.Helpers
 
             return roles;
         }
-
-        /// <summary>
-        /// Checks whether the users passes the whitelist and blacklist role set.
-        /// </summary>
-        public static bool CheckWhitelistBlacklistRoles (SocketGuildUser user, string [] whitelistRoles, string [] blacklistRoles)
-        {
-            bool canRun = false;
-
-            if (whitelistRoles == null ||
-                whitelistRoles.Length == 0 ||
-                whitelistRoles.Contains ("@everyone"))
-            {
-                canRun = true;
-            }
-            else
-            {
-                foreach (var role in user.Roles)
-                    if (whitelistRoles.Contains (role.Name))
-                        canRun = true;
-            }
-
-            if (blacklistRoles == null ||
-                blacklistRoles.Length == 0)
-            {
-                canRun = true;   
-            }
-            else if (blacklistRoles.Contains ("@everyone"))
-            {
-                canRun = false;
-            }
-            else
-            {
-                foreach (var role in user.Roles)
-                    if (blacklistRoles.Contains (role.Name))
-                        canRun = false;
-            }
-
-            return canRun;
-        }
     }
 }
