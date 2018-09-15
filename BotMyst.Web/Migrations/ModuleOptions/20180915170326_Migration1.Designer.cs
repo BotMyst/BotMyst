a00bcf44613672ff265495e229255b8b6f308a60
Naggerknife <messer185@gmail.com>
2018-09-15 13:10:59 -0400

    bd mirgrations

--- BotMyst.Web/Migrations/ModuleOptions/20180915170326_Migration1.Designer.cs
+++ BotMyst.Web/Migrations/ModuleOptions/20180915170326_Migration1.Designer.cs
@@ -2,14 +2,16 @@
 using BotMyst.Web.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BotMyst.Web.Migrations.ModuleOptions
 {
     [DbContext(typeof(ModuleOptionsContext))]
-    partial class ModuleOptionsContextModelSnapshot : ModelSnapshot
+    [Migration("20180915170326_Migration1")]
+    partial class Migration1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
