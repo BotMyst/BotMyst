ab6804471539b18d9000247fa80cf215c11d067c
CodeMyst <codemyst@outlook.com>
2018-10-02 18:35:35 +0200

    Added search to command list

--- BotMyst.Web/Controllers/CommandListController.cs
+++ BotMyst.Web/Controllers/CommandListController.cs
@@ -1,13 +1,13 @@
+using System.Linq;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 using Microsoft.AspNetCore.Mvc;
 
 using BotMyst.Web.Models;
 using BotMyst.Web.Discord;
-using BotMyst.Web.DatabaseContexts;
 using BotMyst.Shared.Models;
-using System.Collections.Generic;
-using System.Linq;
+using BotMyst.Web.DatabaseContexts;
 
 namespace BotMyst.Web.Controllers
 {
@@ -20,8 +20,10 @@ namespace BotMyst.Web.Controllers
             this.moduleDescriptionsContext = moduleDescriptions;
         }
 
-        public async Task<IActionResult> Index (ulong guildId)
+        public async Task<IActionResult> Index (ulong guildId, string commandSearch)
         {
+            ViewData ["CurrentFilter"] = commandSearch;
+
             CommandList model = new CommandList ();
             model.ModuleDescriptions = new List<ModuleDescription> ();
 
@@ -35,6 +37,14 @@ namespace BotMyst.Web.Controllers
                     Name = module.Name,
                     CommandDescriptions = moduleDescriptionsContext.CommandDescriptions.Where (c => c.ModuleDescriptionID == module.ID).ToList ()
                 };
+
+                if (string.IsNullOrEmpty (commandSearch) == false)
+                    moduleDescriptionCopy.CommandDescriptions = moduleDescriptionCopy.CommandDescriptions.Where (c => c.Command.ToLower ().Contains (commandSearch.ToLower ())).ToList ();
+
+                // If it doesn't have any command (probably because the search), don't show this module in the page
+                if (moduleDescriptionCopy.CommandDescriptions.Count == 0)
+                    continue;
+
                 model.ModuleDescriptions.Add (moduleDescriptionCopy);
             }
 
