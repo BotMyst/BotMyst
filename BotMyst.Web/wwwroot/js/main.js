ec51ebf8807ce95976ef02f958234f1089579dff
CodeMyst <codemyst@outlook.com>
2018-10-18 15:27:26 +0200

    Blob picker design

--- BotMyst.Web/wwwroot/js/main.js
+++ BotMyst.Web/wwwroot/js/main.js
@@ -1,4 +1,4 @@
-var navigationActiveClassName = "navigationActive";
+const navigationActiveClassName = "navigationActive";
 
 function toggleNavigation ()
 {
@@ -11,5 +11,48 @@ function toggleNavigation ()
     else
     {
         nav.classList.add (navigationActiveClassName);
+    }
+}
+
+function toggleCommand (guildId, commandId)
+{
+    $.ajax
+    ({
+        url: "/CommandOptions/ToggleCommand?guildId=" + guildId + "&commandId=" + commandId,
+        success: function (data)
+        {
+            var button = $('.commandEnabled a');
+            if (data === true)
+            {
+                button.text ('Enabled');
+                button.removeClass ('disabled');
+                button.addClass ('enabled');
+            }
+            else
+            {
+                button.text ('Disabled');
+                button.removeClass ('enabled');
+                button.addClass ('disabled');
+            }
+        }
+    });
+}
+
+function openBlobPicker ()
+{
+    var blobPicker = document.getElementById ('blobPicker');
+    var close = blobPicker.getElementsByClassName ('close') [0];
+
+    blobPicker.style.display = 'block';
+
+    close.onclick = function ()
+    {
+        blobPicker.style.display = 'none';
+    }
+
+    window.onclick = function (event)
+    {
+        if (event.target == blobPicker)
+            blobPicker.style.display = 'none';
     }
 }
