b74d791905b5ad4a2375c823490b890fa1d55b9c
CodeMyst <codemyst@outlook.com>
2018-10-22 15:07:59 +0200

    Role list displays roles instead of dummy roles

--- BotMyst.Web/Discord/DiscordExtensions.cs
+++ BotMyst.Web/Discord/DiscordExtensions.cs
@@ -1,4 +1,5 @@
 using System.Linq;
+using System.Threading.Tasks;
 using System.Collections.Generic;
 
 using BotMyst.Web.Discord.Models;
@@ -12,5 +13,16 @@ namespace BotMyst.Web.Discord
 
         public static bool HasAdministratorPermission (this DiscordGuild guild) =>
             (guild.Permissions & 8) == 8;
+
+        public static async Task<IEnumerable<DiscordRole>> ToDiscordRolesAsync (this string input, ulong guildId)
+        {
+            if (string.IsNullOrEmpty (input)) return null;
+
+            string [] roles = input.Split (',');
+
+            DiscordGuild guild = await DiscordAPI.GetBotGuildAsync (guildId);
+
+            return guild.Roles.Where (r => roles.Contains (r.Name));
+        }
     }
 }
